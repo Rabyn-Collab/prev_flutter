b929fa83f16203089089f72aeb03d60f240c5a1a
Rabyn <rabyn900@gmail.com>
2022-05-17 08:37:25 +0545

    changes

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_start/pages/main_page.dart';
+
+
+
+//
+// Future<String> getData() async{
+//   print('hello');
+//   await Future.delayed(Duration(seconds: 2));
+//   return 'hello user k cha';
+//   // throw Exception('some error');
+// }
+
+
+void main() async {
+ // getData().then((value) => print(value)).catchError((err) => print(err));
+ //  try{
+ //    final data = await getData();
+ //    print(data);
+ //  }catch (err){
+ //    print('err cha $err');
+ //  }
+
+  runApp(ProviderScope(child: Home()));
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
